aee521be24510a044bb444f6abee6f30bff5ab14
Sawaiz Naveed <sawaiznaveed1@gmail.com>
2023-12-31 22:15:48 +0100

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Sawaiz Naveed on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
