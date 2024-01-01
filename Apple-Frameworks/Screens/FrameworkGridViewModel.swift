e5b701a75d0a213453582de015732f10e7bbb91c
Sawaiz Naveed <sawaiznaveed1@gmail.com>
2024-01-01 20:19:09 +0100

    Project Organization

--- Apple-Frameworks/Screens/FrameworkGridViewModel.swift
+++ Apple-Frameworks/Screens/FrameworkGridViewModel.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 final class FrameworkGridViewModel: ObservableObject {
-    
+    let columns: [GridItem] = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     var selectedFramework: Framework?
     {
         didSet{
